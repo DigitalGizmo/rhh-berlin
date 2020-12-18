c2130fc513eb75cd0b5affe46b27a3f9fcf8b255
Don Button <donpublic@digitalgizmo.com>
2020-12-18 11:22:16 -0500

    Related Files Launch

--- common/header.php
+++ common/header.php
@@ -36,7 +36,6 @@
     <?php echo head_js(); ?>
 
     <!-- Load js the way Erin did in curatescape to get real-time photoswipe -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
         /*!
         loadJS: load a JS file asynchronously. 
